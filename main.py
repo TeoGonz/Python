e5e26d2ae74767d5ef78320d76c1725ce8a6021d
MateoGonz <mateo.grisales@aossas.com>
2024-02-25 14:58:56 -0500

    Fix - implements test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from resources.menu.main_menu import MainMenu
 
 class Main:
     def main(self):
+        print('mainMenu python is executed')
         MainMenu().main_menu()
 #   Ejecución del programa
 Main().main()
