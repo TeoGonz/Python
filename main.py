00bebffd6d674782c86d56e45afe06c660c8294c
MateoGonz <matt.grisa@outlook.com>
2024-02-25 14:08:58 -0500

    Fix - implements - menus

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-import sys
-from listas_enlazadas.lists_menu import ListMenu
+from resources.menu.main_menu import MainMenu
 
 class Main:
-    def __init__(self):
-        self.__lists_menu = ListMenu()
+    # def __init__(self):
+    #     self.__lists_menu = ListMenu()
 
     def main(self):
-        print('Bienvenido al menú')
-        print('¿Que desea revisar?')
-        option_main = int(input(
-                'Para ejecutar el código de listas dobles enlazadas:    Presione [1]\n'
-                'Para salir de la aplicación:                           Presione [2]\n'
-            ))
-        options_main = {
-            1: self.__lists_menu.print_lists_menu,
-            2: exit
-        }
-        options_main.get(option_main, exit)() # Usando recursividad para volver a imprimir el menú
+        MainMenu().main_menu()
 
 #   Ejecución del programa
 Main().main()
--- resources/menu/lists_menu.py
+++ resources/menu/lists_menu.py
@@ -1,3 +1,4 @@
+import os
 from listas_enlazadas.client_list import ClientList
 
 class ListMenu:
@@ -9,46 +10,35 @@ class ListMenu:
         self.__list_module = ClientList() 
         self.__list_module.create_double_linked_list()
         print('✅ The list has been created successfully\n\n')
-        return self.print_lists_menu()
+        return
     
     def print_all_double_linked_list(self):
         if(self.__list_module is None):
+            os.system('clear')
             print(self.__empty_message)
-            return self.print_lists_menu()
+            return None
         self.__list_module.print_all_double_linked_list()
-        return self.print_lists_menu()            
     
     def print_women_age_average(self):
         if(self.__list_module is None):
+            os.system('clear')
             print(self.__empty_message)
-            return self.print_lists_menu()
+            return None
         self.__list_module.print_women_age_average()
-        return self.print_lists_menu()
     
     def print_men_age_average(self):
         if(self.__list_module is None):
+            os.system('clear')
             print(self.__empty_message)
-            return self.print_lists_menu()
+            return None            
         self.__list_module.print_men_age_average()
-        return self.print_lists_menu()
     
-    def print_lists_menu(self):
-        print('\n\n\n')
-        print('Menú de listas dobles enlazadas')
-        print('¿Que desea hacer?\n')
-        option_lists = int(input(
-                'Create a double linked list:   press [1]\n'
-                'Print all elements in list:    press [2]\n'
-                'Print women age average:       press [3]\n'
-                'Print men age average:         press [4]\n'
-                'Exit the app:                  Press [5]\n'
-            ))
-        print('\n\n\n')
+    def event_manager(self, option_selected: int):
         options_lists = {
             1: self.create_linked_list,
             2: self.print_all_double_linked_list,
             3: self.print_women_age_average,
             4: self.print_men_age_average,
-            # 5: self.main
         }        
-        options_lists.get(option_lists, self.print_lists_menu)() # Usando recursividad para volver a imprimir el menú
+        event = options_lists.get(option_selected, None)() # Usando recursividad para volver a imprimir el menú
+        return event
